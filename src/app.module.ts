088c2890edff8e50bd1931c9d3ecd36a9a9cd632
Eduard Tsatourov <eduard.tsatourov@ordina.be>
2021-10-13 13:12:03 +0200

    installs typeorm and connects to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [],
+      synchronize: process.env.MODE === 'DEV',
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
